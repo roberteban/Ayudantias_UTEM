93ffd4ccc121dfa4d2dd5370e8eb3f27376774e1
Roberto Castillo <38573736+Roberteban@users.noreply.github.com>
2023-12-27 19:24:51 -0300

    Octavo commit

--- fronted/src/components/footer/Footer.jsx
+++ fronted/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer-text">Developed by <span className="footer-username">@katarro</span></p>
+      <p className="footer-text">Developed by <span className="footer-username">@roberteban</span></p>
     </footer>
   );
 }
